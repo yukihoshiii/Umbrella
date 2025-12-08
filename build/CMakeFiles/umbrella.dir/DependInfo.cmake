1eb66d086664ce7c73fa6074ca7d118a190502e6
yukihoshi <yuki@yukihoshi.local>
2025-12-08 09:30:34 +0200

    Add build artifacts and vscode settings as requested

---
+++ build/CMakeFiles/umbrella.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/yuki/umbrella/src/compiler/ast.cpp" "CMakeFiles/umbrella.dir/src/compiler/ast.cpp.o" "gcc" "CMakeFiles/umbrella.dir/src/compiler/ast.cpp.o.d"
+  "/Users/yuki/umbrella/src/compiler/codegen.cpp" "CMakeFiles/umbrella.dir/src/compiler/codegen.cpp.o" "gcc" "CMakeFiles/umbrella.dir/src/compiler/codegen.cpp.o.d"
+  "/Users/yuki/umbrella/src/compiler/lexer.cpp" "CMakeFiles/umbrella.dir/src/compiler/lexer.cpp.o" "gcc" "CMakeFiles/umbrella.dir/src/compiler/lexer.cpp.o.d"
+  "/Users/yuki/umbrella/src/compiler/parser.cpp" "CMakeFiles/umbrella.dir/src/compiler/parser.cpp.o" "gcc" "CMakeFiles/umbrella.dir/src/compiler/parser.cpp.o.d"
+  "/Users/yuki/umbrella/src/runtime/advanced.cpp" "CMakeFiles/umbrella.dir/src/runtime/advanced.cpp.o" "gcc" "CMakeFiles/umbrella.dir/src/runtime/advanced.cpp.o.d"
+  "/Users/yuki/umbrella/src/runtime/runtime.cpp" "CMakeFiles/umbrella.dir/src/runtime/runtime.cpp.o" "gcc" "CMakeFiles/umbrella.dir/src/runtime/runtime.cpp.o.d"
+  "/Users/yuki/umbrella/src/umbrella.cpp" "CMakeFiles/umbrella.dir/src/umbrella.cpp.o" "gcc" "CMakeFiles/umbrella.dir/src/umbrella.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
